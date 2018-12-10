d79e74c6f7f58bd0988473c967cf117dc731e592
Lindhauer <Lindhauer@his.de>
2018-12-10 12:33:24 +0100

    Initiale Erstellung. Damit das sinnvoll eingesetzt werden kann ist aber z.B. die Berechnung der Entfernung notwendig.
    Signed-off-by: Lindhauer <Lindhauer@his.de>

---
+++ src/tryout/algorithm/shortestPath/bellmannFord/BellmanFord.java
@@ -0,0 +1,142 @@
+package tryout.algorithm.shortestPath.bellmannFord;
+
+import java.util.HashMap;
+import java.util.Scanner;
+ 
+/**ursprünglich aus: https://java2blog.com/bellman-ford-algorithm-java/?utm_source=feedburner&utm_medium=email&utm_campaign=Feed%3A+ArpitMandliyasBlog+(Arpit+Mandliya%27s+Blog)
+ * @author lindhauer
+ *
+ */
+public class BellmanFord {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int nodes = scn.nextInt();
+		int[][] graph = new int[nodes + 1][nodes + 1];
+		int numEdges = scn.nextInt();
+		for (int edge = 0; edge < numEdges; edge++) {
+			int u = scn.nextInt(), v = scn.nextInt(), w = scn.nextInt();
+			graph[u][v] = w;
+		}
+ 
+		bellmanford(scn.nextInt(), nodes, numEdges, graph);
+	}
+ 
+public static edge[] getEdges(int numEdges, int[][] graph) {
+		edge[] rv = new edge[numEdges];
+ 
+		int idx = 0;
+		for (int u = 1; u < graph.length && idx < rv.length; u++) {
+		for (int v = 1; v < graph[u].length && idx < rv.length; v++) {
+				rv[idx] = new edge(u, v);
+				idx = graph[u][v] != 0 ? idx + 1 : idx;
+			}
+		}
+ 
+		return rv;
+ 
+	}
+
+	public static class edge {
+		int u;
+		int v;
+ 
+		public edge(int u, int v) {
+			this.u = u;
+			this.v = v;
+		}
+ 
+		public String toString() {
+			return this.u + " " + this.v;
+		}
+	}
+ 
+	public static class Node {
+		int val;
+		int dist;
+		String path;
+ 
+		public Node(int val, int dist, String path) {
+			this.val = val;
+			this.dist = dist;
+			this.path = path;
+		}
+ 
+		public String toString() {
+			return "Distance of " + this.val + " from source is " + this.dist + " and path followed is " + this.path;
+		}
+ 
+	}
+ 
+
+	
+		public static void bellmanford(int src, int nodes, int edges, int[][] graph) {
+			/*
+			 * we use hashmap to store the nodes of every vertex,
+			 * (vertex name, node) will be the key, value pair
+			 */
+			HashMap<Integer, Node> nodesMap = new HashMap<>();
+			for (int i = 1; i < graph.length; i++) {
+				/*
+				 * initialize the shortest distance of the every
+				 *  vertex equal to Infinity as for this vertex 
+				 *  the shortest distance is yet to be calculated,
+				 *  and initialize an empty path. But if the vertex 
+				 *  is source vertex itself, then the shortest distance
+				 *  for it will be 0 and path will be initialized with 
+				 *  vertex name.
+				 */
+	nodesMap.put(i, new Node(i, i == src ? 0 : (int) 1e9 + 1, i == src ? 
+	                Integer.toString(i) : ""));
+			}
+	 
+			/* outer loop will run for vertices - 1 times */
+			for (int var = 1; var <= nodes - 1; var++) {
+				/* running inner loop on the set of edges returned 
+				 * from getEdges function */
+				for (edge e : getEdges(edges, graph)) {
+					Node u = nodesMap.get(e.u);
+					Node v = nodesMap.get(e.v);
+	 
+					/*
+					 * the basic condition for updation of shortest
+					 * distance of any node as mentioned in the above 
+					 * discussion.
+					 */
+					if (v.dist > u.dist + graph[u.val][v.val]) {
+						v.dist = u.dist + graph[u.val][v.val];
+						v.path = u.path + "->" + Integer.toString(v.val);
+					}
+				}
+			}
+	 
+			/*
+			 * one more loop in the random set of edges to detect if
+			 *  there is any negative cycle or not
+			 */
+			for (edge e : getEdges(edges, graph)) {
+				Node u = nodesMap.get(e.u);
+				Node v = nodesMap.get(e.v);
+	 
+				/*
+				 * if the we still are able to find shorted distance
+				 * this simply means that there is a negative cycle
+				 * for sure and hence we return from the function as
+				 * shortest distance for every vertex from source can
+				 * not be found for such graph as we can get even 
+				 * shorter distance by looping once again in that
+				 * negative cycle.
+				 */
+				if (v.dist > u.dist + graph[u.val][v.val]) {
+					System.out.println("Negative Cycle Detected");
+					return;
+				}
+			}
+	 
+			for (int node : nodesMap.keySet()) {
+				System.out.println(nodesMap.get(node));
+			}
+	 
+		}
+	 
+	}
---
+++ src/tryout/algorithm/shortestPath/dijkstra/TryoutShortestPathMain.java
@@ -0,0 +1,81 @@
+package tryout.algorithm.shortestPath.dijkstra;
+
+import java.util.PriorityQueue;
+
+/**Übernommen aus https://java2blog.com/dijkstra-java/
+ * @author lindhauer
+ *
+ */
+public class TryoutShortestPathMain {
+
+	public static void main(String[] args) {
+		Vertex vertexA = new Vertex("A");
+		Vertex vertexB = new Vertex("B");
+		Vertex vertexC = new Vertex("C");
+		Vertex vertexD = new Vertex("D");
+		Vertex vertexE = new Vertex("E");
+		
+		vertexA.addNeighbour(new Edge(10,vertexA,vertexC));
+		vertexA.addNeighbour(new Edge(17,vertexA,vertexB));
+		vertexC.addNeighbour(new Edge(5,vertexC,vertexB));
+		vertexC.addNeighbour(new Edge(9,vertexC,vertexD));
+		vertexC.addNeighbour(new Edge(11,vertexC,vertexE));
+		vertexB.addNeighbour(new Edge(1,vertexB,vertexD));
+		vertexD.addNeighbour(new Edge(6,vertexD,vertexE));
+	
+		DijkstraShortestPath shortestPath = new DijkstraShortestPath();
+		shortestPath.computeShortestPaths(vertexA);
+		
+		System.out.println("======================================");
+		System.out.println("Calculating minimum distance");
+		System.out.println("======================================");
+		
+		System.out.println("Minimum distance from A to B: "+vertexB.getDistance());
+		System.out.println("Minimum distance from A to C: "+vertexC.getDistance());
+		System.out.println("Minimum distance from A to D: "+vertexD.getDistance());
+		System.out.println("Minimum distance from A to E: "+vertexE.getDistance());
+		
+		System.out.println("=====================	=================");
+		System.out.println("Calculating Paths");
+		System.out.println("======================================");
+		
+		System.out.println("Shortest Path from A to B: "+shortestPath.getShortestPathTo(vertexB));
+		System.out.println("Shortest Path from A to C: "+shortestPath.getShortestPathTo(vertexC));
+		System.out.println("Shortest Path from A to D: "+shortestPath.getShortestPathTo(vertexD));
+		System.out.println("Shortest Path from A to E: "+shortestPath.getShortestPathTo(vertexE));
+		
+		
+	}
+	
+	
+	public void computeShortestPaths(Vertex sourceVertex){
+		 
+		sourceVertex.setDistance(0);
+		PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>();
+		priorityQueue.add(sourceVertex);
+		sourceVertex.setVisited(true);
+ 
+		while( !priorityQueue.isEmpty() ){
+            // Getting the minimum distance vertex from priority queue
+			Vertex actualVertex = priorityQueue.poll();
+ 
+			for(Edge edge : actualVertex.getAdjacenciesList()){
+ 
+				Vertex v = edge.getTargetVertex();
+				if(!v.isVisited())
+				{
+					double newDistance = actualVertex.getDistance() + edge.getWeight();
+ 
+					if( newDistance < v.getDistance() ){
+						priorityQueue.remove(v);
+						v.setDistance(newDistance);
+						v.setPredecessor(actualVertex);
+						priorityQueue.add(v);
+					}
+				}
+			}
+			actualVertex.setVisited(true);
+		}
+	}
+
+}
