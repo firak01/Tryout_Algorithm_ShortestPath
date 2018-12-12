580572bce0764225b509c66dec6e666bcd326286
firak01 <firak01@t-online.de>
2018-12-12 10:31:47 +0100

    Das blockieren eines Wegs funktioniert, indem man den Knoten aus dem Weg herausnimmt.
    Signed-off-by: firak01 <firak01@t-online.de>

--- src/tryout/algorithm/shortestPath/dijkstra002/TryoutShortestPathMain.java
+++ src/tryout/algorithm/shortestPath/dijkstra002/TryoutShortestPathMain.java
@@ -127,6 +127,33 @@ public class TryoutShortestPathMain {
 					}
 					}
 			
+			//#################
+			from5_4_blocked:{
+						//Zieh ein Netz aus Knoten auf, FÜR JEDE AUSGANGSPOSITION neu, da wohl intern gespeichert wird, ob der Knoten schon besucht worden ist.
+							HashMapMultiZZZ hmVertex = fillHexMapNavigationMapBlocked();
+							HexVertex objVertexStart = (HexVertex) hmVertex.get("5","4");
+								
+							DijkstraShortestPath shortestPathFrom = new DijkstraShortestPath();
+							shortestPathFrom.computeShortestPaths(objVertexStart);
+							
+							System.out.println("======================================");
+							System.out.println("Calculating minimum distance from 5/4");
+							System.out.println("======================================");
+									
+							HexVertex objVertex2_5 = (HexVertex) hmVertex.get("2","5");
+							System.out.println("Minimum distance from 5/4 to 2/5: "+objVertex2_5.getDistance());
+							
+							
+							System.out.println("======================================");
+							System.out.println("Calculating Paths");
+							System.out.println("======================================");
+							List<HexVertex> listPath = shortestPathFrom.getShortestPathTo(objVertex2_5);
+							System.out.println("Shortest Path from 5/4 to 2/5 geht über "+listPath.size() + " Knoten.");
+							for(HexVertex objPathTemp : listPath){
+								System.out.println(objPathTemp.toString());
+							}
+							}
+			
 			//###########		
 		}catch(ExceptionZZZ ez){
 			ez.printStackTrace();
@@ -211,114 +238,206 @@ public class TryoutShortestPathMain {
 	
 	public static HashMapMultiZZZ fillHexMapNavigationMap() throws ExceptionZZZ{
 		HashMapMultiZZZ objReturn = new HashMapMultiZZZ();  //Hashmap mit Hashmap, in der die Sechseckzellen in Form von Koordinaten (z.B. "1","1") abgelegt sind.
-		
-		//1. Erstelle die Knoten
-		for(int iX=1; iX<=9; iX++){			
-			for(int iY=1; iY<=7;iY++){
-				
-				Integer intX = new Integer(iX);				
-				String sX = intX.toString();
-				
-				Integer intY = new Integer(iY);
-				String sY = intY.toString();
-				
-				HexVertex objVertex = new HexVertex(sX, sY);
-								
-				objReturn.put(sX, sY, objVertex);
-				
-			}// end for iY
+		main:{
+			//1. Erstelle die Knoten
+			int iVertexTotal = fillHexVertexMap(objReturn);
+			if(iVertexTotal==0)break main;
 			
-		}//end for iX
-		
-		
-		//2. Mache die Verküpfungen
-		//Merke: Die Entfernung der Knoten beträgt immer 1, s. Konstruktor von "Edge".
-		for(int iX=1; iX<=9; iX++){			
-			for(int iY=1; iY<=7;iY++){
-				
-				String sX = Integer.toString(iX);
-				String sY = Integer.toString(iY);
-				System.out.println("+++++++++++++++");
-				System.out.println("+++ Definiere Vernüpfungen von Knoten " + sX + "/" + sY);
-				HexVertex objVertex = (HexVertex) objReturn.get(sX, sY);
-				
-				//Hole die 6 Nachbarknoten, bzw. Kanten dorthin.
-				
-				//1++++				
-				int iXtemp = iX;
-				int iYtemp = iY-1;
-				String sXtemp = Integer.toString(iXtemp);
-				String sYtemp = Integer.toString(iYtemp);
-				
-				HexVertex objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
-				if(isNeighbor(objVertex, objVertexTemp)){
-					System.out.println("1... nach Knoten " + sXtemp + "/" + sYtemp);
-					Edge edge = new Edge(1,objVertex, objVertexTemp);
-					objVertex.addNeighbour(edge);
-				}
 			
+			//2. Mache die Verküpfungen
+			int iEdgeTotal = fillHexEdges(objReturn);
+			if(iEdgeTotal==0)break main;
+		}//end main:
+		return objReturn;
+	}
+	
+	public static int fillHexVertexMap(HashMapMultiZZZ objReturn) throws ExceptionZZZ{
+		int iReturn = 0;
+		main:{
+			if(objReturn==null) break main;
+			
+			//1. Erstelle die Knoten
+			for(int iX=1; iX<=9; iX++){			
+				for(int iY=1; iY<=7;iY++){
+					
+					Integer intX = new Integer(iX);				
+					String sX = intX.toString();
+					
+					Integer intY = new Integer(iY);
+					String sY = intY.toString();
+					
+					HexVertex objVertex = new HexVertex(sX, sY);
+									
+					objReturn.put(sX, sY, objVertex);
+					iReturn++;
+				}// end for iY
 				
-				//2++++
-				iXtemp = iX+1;
-				iYtemp = iY-1;
-				sXtemp = Integer.toString(iXtemp);
-				sYtemp = Integer.toString(iYtemp);
-				objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
-				if(isNeighbor(objVertex, objVertexTemp)){	
-					System.out.println("2... nach Knoten " + sXtemp + "/" + sYtemp);
-					Edge edge = new Edge(1,objVertex, objVertexTemp);
-					objVertex.addNeighbour(edge);
-				}	
-//				
-//				//3++++
-				iXtemp = iX+1;
-				iYtemp = iY;
-				sXtemp = Integer.toString(iXtemp);
-				sYtemp = Integer.toString(iYtemp);
-				objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
-				if(isNeighbor(objVertex, objVertexTemp)){	
-					System.out.println("3... nach Knoten " + sXtemp + "/" + sYtemp);
-					Edge edge = new Edge(1,objVertex, objVertexTemp);
-					objVertex.addNeighbour(edge);
-				}	
-//				
-//				//4++++
-				iXtemp = iX+1;
-				iYtemp = iY+1;
-				sXtemp = Integer.toString(iXtemp);
-				sYtemp = Integer.toString(iYtemp);
-				objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
-				if(isNeighbor(objVertex, objVertexTemp)){	
-					System.out.println("4... nach Knoten " + sXtemp + "/" + sYtemp);
-					Edge edge = new Edge(1,objVertex, objVertexTemp);
-					objVertex.addNeighbour(edge);
-				}	
-//				
-//				//5++++
-				iXtemp = iX;
-				iYtemp = iY+1;
-				sXtemp = Integer.toString(iXtemp);
-				sYtemp = Integer.toString(iYtemp);
-				objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
-				if(isNeighbor(objVertex, objVertexTemp)){	
-					System.out.println("5... nach Knoten " + sXtemp + "/" + sYtemp);
-					Edge edge = new Edge(1,objVertex, objVertexTemp);
-					objVertex.addNeighbour(edge);
-				}	
-//				
-//				//6++++
-				iXtemp = iX-1;
-				iYtemp = iY;
-				sXtemp = Integer.toString(iXtemp);
-				sYtemp = Integer.toString(iYtemp);
-				objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
-				if(isNeighbor(objVertex, objVertexTemp)){	
-					System.out.println("6... nach Knoten " + sXtemp + "/" + sYtemp);
-					Edge edge = new Edge(1,objVertex, objVertexTemp);
-					objVertex.addNeighbour(edge);
-				}																	
-			}// end for iY
-		}//end for iX
+			}//end for iX
+		}
+		return iReturn;
+	}
+	
+	public static int blockHexVertex(HashMapMultiZZZ objReturn) throws ExceptionZZZ{
+		int iReturn = 0;
+		main:{
+			if(objReturn==null) break main;
+			
+			//1. Erstelle die Knoten
+			for(int iX=1; iX<=9; iX++){			
+				for(int iY=1; iY<=7;iY++){
+					
+					if(iX==4 && iY==4){
+						Integer intX = new Integer(iX);				
+						String sX = intX.toString();
+						
+						Integer intY = new Integer(iY);
+						String sY = intY.toString();
+						
+						Object objFound = objReturn.remove(sX,sY);							
+						//objReturn.put(sX, sY, objVertex);
+						if(objFound!=null){
+							iReturn++;
+						}
+					}
+					
+					if(iX==5 && iY==5){
+						Integer intX = new Integer(iX);				
+						String sX = intX.toString();
+						
+						Integer intY = new Integer(iY);
+						String sY = intY.toString();
+						
+						Object objFound = objReturn.remove(sX,sY);							
+						//objReturn.put(sX, sY, objVertex);
+						if(objFound!=null){
+							iReturn++;
+						}
+					}
+				}// end for iY
+				
+			}//end for iX
+		}//end main:
+		return iReturn;
+	}
+	
+	public static int fillHexEdges(HashMapMultiZZZ objReturn) throws ExceptionZZZ{
+		int iReturn = 0;
+		main:{
+			//Merke: Die Entfernung der Knoten beträgt immer 1, s. Konstruktor von "Edge".
+			for(int iX=1; iX<=9; iX++){			
+				for(int iY=1; iY<=7;iY++){
+					
+					String sX = Integer.toString(iX);
+					String sY = Integer.toString(iY);
+					System.out.println("+++++++++++++++");
+					System.out.println("+++ Definiere Vernüpfungen von Knoten " + sX + "/" + sY);
+					HexVertex objVertex = (HexVertex) objReturn.get(sX, sY);
+					
+					//Hole die 6 Nachbarknoten, bzw. Kanten dorthin.
+					
+					//1++++				
+					int iXtemp = iX;
+					int iYtemp = iY-1;
+					String sXtemp = Integer.toString(iXtemp);
+					String sYtemp = Integer.toString(iYtemp);
+					
+					HexVertex objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
+					if(isNeighbor(objVertex, objVertexTemp)){
+						System.out.println("1... nach Knoten " + sXtemp + "/" + sYtemp);
+						Edge edge = new Edge(1,objVertex, objVertexTemp);
+						objVertex.addNeighbour(edge);
+						iReturn++;
+					}
+				
+					
+					//2++++
+					iXtemp = iX+1;
+					iYtemp = iY-1;
+					sXtemp = Integer.toString(iXtemp);
+					sYtemp = Integer.toString(iYtemp);
+					objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
+					if(isNeighbor(objVertex, objVertexTemp)){	
+						System.out.println("2... nach Knoten " + sXtemp + "/" + sYtemp);
+						Edge edge = new Edge(1,objVertex, objVertexTemp);
+						objVertex.addNeighbour(edge);
+						iReturn++;
+					}	
+//					
+//					//3++++
+					iXtemp = iX+1;
+					iYtemp = iY;
+					sXtemp = Integer.toString(iXtemp);
+					sYtemp = Integer.toString(iYtemp);
+					objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
+					if(isNeighbor(objVertex, objVertexTemp)){	
+						System.out.println("3... nach Knoten " + sXtemp + "/" + sYtemp);
+						Edge edge = new Edge(1,objVertex, objVertexTemp);
+						objVertex.addNeighbour(edge);
+						iReturn++;
+					}	
+//					
+//					//4++++
+					iXtemp = iX+1;
+					iYtemp = iY+1;
+					sXtemp = Integer.toString(iXtemp);
+					sYtemp = Integer.toString(iYtemp);
+					objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
+					if(isNeighbor(objVertex, objVertexTemp)){	
+						System.out.println("4... nach Knoten " + sXtemp + "/" + sYtemp);
+						Edge edge = new Edge(1,objVertex, objVertexTemp);
+						objVertex.addNeighbour(edge);
+						iReturn++;
+					}	
+//					
+//					//5++++
+					iXtemp = iX;
+					iYtemp = iY+1;
+					sXtemp = Integer.toString(iXtemp);
+					sYtemp = Integer.toString(iYtemp);
+					objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
+					if(isNeighbor(objVertex, objVertexTemp)){	
+						System.out.println("5... nach Knoten " + sXtemp + "/" + sYtemp);
+						Edge edge = new Edge(1,objVertex, objVertexTemp);
+						objVertex.addNeighbour(edge);
+						iReturn++;
+					}	
+//					
+//					//6++++
+					iXtemp = iX-1;
+					iYtemp = iY;
+					sXtemp = Integer.toString(iXtemp);
+					sYtemp = Integer.toString(iYtemp);
+					objVertexTemp = (HexVertex) objReturn.get(sXtemp, sYtemp);
+					if(isNeighbor(objVertex, objVertexTemp)){	
+						System.out.println("6... nach Knoten " + sXtemp + "/" + sYtemp);
+						Edge edge = new Edge(1,objVertex, objVertexTemp);
+						objVertex.addNeighbour(edge);
+						iReturn++;
+					}																	
+				}// end for iY
+			}//end for iX
+		}
+		return iReturn;
+	}
+	
+	public static HashMapMultiZZZ fillHexMapNavigationMapBlocked() throws ExceptionZZZ{
+		HashMapMultiZZZ objReturn = new HashMapMultiZZZ();  //Hashmap mit Hashmap, in der die Sechseckzellen in Form von Koordinaten (z.B. "1","1") abgelegt sind.
+		main:{
+				
+			
+			//1. Erstelle die Knoten
+			int iVertexTotal = fillHexVertexMap(objReturn);
+			if(iVertexTotal==0)break main;
+			
+			//#### DAS BLOCKEN: Entferne Knoten, die ja ggfs. nicht zur Verfügung stehene
+			int iVertexBlocke = blockHexVertex(objReturn);
+			
+					
+			//2. Mache die Verküpfungen
+			int iEdgeTotal = fillHexEdges(objReturn);
+			if(iEdgeTotal==0)break main;
+		
+		}//end main:
 		return objReturn;
 	}
 
